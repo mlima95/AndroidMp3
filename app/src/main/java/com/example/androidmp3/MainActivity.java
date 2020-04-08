d5ca2cf607efcf26915f2903c18d53a69f1a2b81
CherelThibaut <55751931+CherelThibaut@users.noreply.github.com>
2020-04-08 10:30:14 +0200

    toolbar

--- app/src/main/java/com/example/androidmp3/MainActivity.java
+++ app/src/main/java/com/example/androidmp3/MainActivity.java
@@ -5,40 +5,36 @@ import androidx.appcompat.widget.Toolbar;
 
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 
 import com.example.androidmp3.fragments.IMusicSelected;
 import com.example.androidmp3.fragments.PlayerFragment;
 import com.example.androidmp3.models.Music;
 
-public class MainActivity extends AppCompatActivity implements IMusicSelected {
+public class MainActivity extends AppCompatActivity {
 
     PlayerFragment playerFragment;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
 
         if (findViewById(R.id.frameLayout) != null) {
             playerFragment = new PlayerFragment();
-            playerFragment.setListener(this);
             getSupportFragmentManager().beginTransaction().add(R.id.frameLayout, playerFragment).commit();
         }
-        playerFragment.setListener(this);
     }
 
     @Override
-    public void onMusicSelected(Music music) {
-
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.nav_menu, menu);
+        return true;
     }
-
 }
